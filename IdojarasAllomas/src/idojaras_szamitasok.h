d4e868c015dc8b61cb01d56d365c9f96cbfb733b
Máté Csizmadia <csizmadiamate22@gmail.com>
2024-01-10 15:52:49 +0100

    Hibás fejlécfile feladathoz.

---
+++ IdojarasAllomas/src/idojaras_szamitasok.h
@@ -0,0 +1,13 @@
+/**
+ * @file idojaras_szamitasok.h
+ * @author Kriszta
+ * @brief Időjárási számításokat végző függvények deklarációi.
+ * 
+ * @copyright Copyright (c) 2024 P2GyK Informatika Zrt.
+ * 
+ */
+
+/**
+ * @brief Celsiusból Fahrenheitbe váltó függvény.
+ */
+int celsius_fahrenheit_atvalto(int);
