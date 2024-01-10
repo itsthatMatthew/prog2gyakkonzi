821012ea0f93e841f97cb2c3092816f7b492acff
Máté Csizmadia <csizmadiamate22@gmail.com>
2024-01-10 15:52:32 +0100

    Befejezetlen forráskód feladathoz.

---
+++ IdojarasAllomas/src/idojaras_szamitasok.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file idojaras_szamitasok.cpp
+ * @author Kriszta
+ * @brief Időjárási számításokat végző függvények definíciói.
+ * 
+ * @copyright Copyright (c) 2024 P2GyK Informatika Zrt.
+ * 
+ */
+
+#include "idojaras_szamitasok.h"
+
+int celsius_fahrenheit_atvalto(int celsius) {
+  return celsius; // FIXME(KRI): nem találtam meg a képletet a függvénytáblában,
+                  //             valaki tudnak segíteni? :(
+}
