93dc6d890044cd5289ea4409ca67f7b733be756e
Máté Csizmadia <csizmadiamate22@gmail.com>
2024-02-13 14:29:32 +0100

    Használaton kívüli.

---
+++ IdojarasAllomas/src/string_resources.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define STRING_RESOURCE constexpr char *
+
+STRING_RESOURCE network_ssid = "";
+STRING_RESOURCE network_pswd = "";
+STRING_RESOURCE network_ntp_server = "";
+
+STRING_RESOURCE display_text_temperature = "Homerseklet";
+STRING_RESOURCE display_text_humidity = "Paratartalom";
+STRING_RESOURCE display_text_heatindex = "Hoindex";
+
+STRING_RESOURCE display_text_temperature_short = "Hom";
+STRING_RESOURCE display_text_humidity_short = "Para";
+STRING_RESOURCE display_text_heatindex_short = "Hoindex";
+
+STRING_RESOURCE display_text_celsius = "C";
+STRING_RESOURCE display_text_fahrenheit = "F";
+STRING_RESOURCE display_text_percentage = "%";
+
+#undef STRING_RESOURCE
